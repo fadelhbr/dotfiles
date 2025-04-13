5269037eefe7ee49c36395c157c4c05b0848f898
fadelhbr <fadeelnfza@gmail.com>
2025-04-13 21:39:50 +0700

    update dotfiles, nust and much more!

---
+++ install.sh
@@ -0,0 +1,1 @@
+sudo pacman -S --needed git hyprpaper waybar dunst libnotify ttf-firacode-nerd ttf-meslo-nerd ttf-jetbrains-mono-nerd pamixer bash-completion cliphist slurp grim github-cli libmtp gvfs-mtp android-tools ntfs-3g noto-fonts-emoji timeshift
