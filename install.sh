acd09cccc3cbfed6ad0006b9b7b580c2c77ef72f
Fadel <fadeelnfza@gmail.com>
2025-04-14 15:50:15 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,2 @@
-sudo pacman -S --needed git hyprpaper waybar dunst libnotify ttf-firacode-nerd ttf-meslo-nerd ttf-jetbrains-mono-nerd pamixer bash-completion cliphist slurp grim github-cli libmtp gvfs-mtp android-tools ntfs-3g noto-fonts-emoji timeshift
+#!/bin/bash
+sudo pacman -S --needed git hyprpaper waybar dunst libnotify ttf-firacode-nerd ttf-meslo-nerd ttf-jetbrains-mono-nerd pamixer bash-completion cliphist slurp grim github-cli libmtp gvfs-mtp android-tools ntfs-3g noto-fonts-emoji ttf-meslo-nerd bleachbit materia-gtk-theme git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
